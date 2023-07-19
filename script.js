85f5b76b27b13ef9cd22d610c9b83212a212c388
Adé Obayomi <86984626+Elapache98@users.noreply.github.com>
2023-07-18 21:57:09 -0400

    UI + logic updates

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function calculateTip() {
     const tipPercentage = parseFloat(document.getElementById("tipPercentage").value);
     const numberOfGuests = parseFloat(document.getElementById("numberOfGuests").value);
 
+
     if (isNaN(billAmount) || isNaN(tipPercentage) || isNaN(numberOfGuests)) {
         alert("Please enter a valid number, thanks.");
         return;
@@ -10,9 +11,11 @@ function calculateTip() {
 
     const tipAmount = (tipPercentage / 100 * billAmount).toFixed(2);
     const totalBill = ((billAmount + parseFloat(tipAmount)) / numberOfGuests).toFixed(2);
+    const eachTip = (tipAmount / numberOfGuests).toFixed(2);
 
     document.getElementById("tipAmount").textContent = `${tipAmount}`;
     document.getElementById("totalBill").textContent = `${totalBill}`;
+    document.getElementById('eachTip').textContent = `${eachTip}`;
 }
 
 function getBill() {
@@ -26,6 +29,9 @@ function clearInput() {
     document.getElementById('billAmount').value = "";
     document.getElementById('tipPercentage').value = "";
     document.getElementById('numberOfGuests').value = "";
+    document.getElementById('tipAmount').textContent = "0.00";
+    document.getElementById('totalBill').textContent = "0.00";
+    document.getElementById('eachTip').textContent = "0.00";
 }
 
 
